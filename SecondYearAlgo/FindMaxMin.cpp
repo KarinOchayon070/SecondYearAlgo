0133c1ecd9820e5c8edfa621cba0fa66089fe03f
karin <karin@DESKTOP-CQ3JVVP>
2021-10-19 21:02:57 +0300

    Add project files.

---
+++ SecondYearAlgo/FindMaxMin.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+/* Practice 1 - 18.10.21:
+Finding the minimum and maximum -  (⌊3n/2⌋ - 2) Comparisons.*/
+
+
+void FindMaxAndMin(int arr[], int n) {
+
+	int Min, Max, MinLocal, MaxLocal, i = 0;
+
+	if (arr[0] < arr[1]) {
+		Min = arr[0];
+		Max = arr[1];
+	}
+	else {
+		Min = arr[1];
+		Max = arr[0];
+	}
+
+	for (i = 2; i < n - 2; i++) {
+		if (arr[i] < arr[i + 1]) {
+			MinLocal = arr[i];
+			MaxLocal = arr[i + 1];
+		}
+		else {
+			MinLocal = arr[i + 1];
+			MaxLocal = arr[i];
+		}
+		if (MinLocal < Min) {
+			Min = MinLocal;
+		}
+		if (MaxLocal > Max) {
+			Max = MaxLocal;
+		}
+	}
+	cout << "Min: " << Min << ", ";
+	cout << "Max: " << Max << ".";
+}
+
+
+int main() {
+	int arr[10] = { 77,-1,9,1,0,7,5,3,2,6 };
+	FindMaxAndMin(arr, 10);
+}
---
+++ SecondYearAlgo/MajorityProblem.cpp
@@ -0,0 +1,81 @@
+﻿#include<iostream>
+using namespace std;
+
+/* Lesson 1 - 17/10/2021:
+Given an array of size n, find the majority element.
+The majority element is the element that appears more than ⌊n/2⌋ times. */
+
+
+//1 - Using two for loops - Time Complexity : O(n2).
+int majorityProblemTwoLoops(int arr[], int n) {
+
+    //Using two for-loops, count the number of appearance for each element.Once the counter exceeds the ⌊n / 2⌋, then we return the element.
+    int i = 0, maxCount = 0, index = -1;
+    for (i = 0; i < n; i++) {
+        int count = 0;
+        for (int j = 0; j < n; j++) {
+            if (arr[i] == arr[j])
+                count++;
+        }
+        // update maxCount if count of current element is greater
+        if (count > maxCount) {
+            maxCount = count;
+            index = i;
+        }
+    }
+    // if maxCount is greater than ⌊n / 2⌋ return the corresponding element
+    if (maxCount > n / 2) {
+        cout << arr[index] << endl;
+    }
+    else {
+        cout << "No Majority Element" << endl;
+        return 0;
+    }
+    return 1;
+}
+
+//2 - Using Moore’s Voting Algorithm - Time Complexity : O(n).
+int majorityProblemMooresVotingAlgo(int arr[], int n) {
+
+    int i = 0, cand = -1, count = 0, sum = 0;
+    for (i = 0; i < n; i++) {
+
+        if (count == 0) {
+            cand = arr[i];
+            count++;
+        }
+        else {
+            if (arr[i] == cand) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+    }
+
+    count = 0;
+    for (i = 0; i < n; i++) {
+        if (arr[i] == cand) {
+            count++;
+        }
+    }
+
+    if (count > n / 2) {
+        cout << cand;
+        return cand;
+    }
+    else {
+        cout << "No";
+        return 0;
+    }
+}
+
+
+int main()
+{
+    int arr[7] = { 2,2,1,1,3,2,2 };
+    int n = 7;
+    //int ansForLoops = majorityProblemTwoLoops(arr, 7);
+    int ansMores = majorityProblemMooresVotingAlgo(arr, 7);
+}
